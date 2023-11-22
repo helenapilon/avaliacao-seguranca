3a466317e5db90592f35bef5fd97c7ea39e3512a
Helena Pilon <138323083+hppilon@users.noreply.github.com>
2023-11-21 21:31:57 -0300

    Update api.ts

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: process.env.API_URL || "https://sistema-academico-api.onrender.com/",
-  // baseURL: "http://localhost:8080/",
-  baseURL: "https://obsidian-nimble-street.glitch.me/",
+  baseURL: "http://localhost:8080/",
+  // baseURL: "https://obsidian-nimble-street.glitch.me/",
   headers: {
     "Content-type": "application/json",
   },
